7d432c72ea6e2cb47e817eb37d8ab79d4f8967f2
Rishi Soni <52169818+rishiso@users.noreply.github.com>
2020-03-02 19:08:15 -0500

    Add files via upload

---
+++ mom7406/src/main/java/frc/robot/PullActuator.java
@@ -0,0 +1,37 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.Servo;
+
+public class PullActuator extends Servo {
+
+    String mode = "CLOSE";
+    
+    public PullActuator(Integer port) {
+        super(port);
+        setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
+    }
+
+    public void open() {
+        setSpeed(1.0);
+    }
+
+    public void close() {
+        setSpeed(-1.0);
+    }
+
+    public void switchMode() {
+        if (mode == "CLOSE") {
+            mode = "OPEN";
+        } else {
+            mode = "CLOSE";
+        }
+    }
+
+    public void activate() {
+        if (mode == "CLOSE") {
+            close();
+        } else {
+            open();
+        }
+    }
+
+}
