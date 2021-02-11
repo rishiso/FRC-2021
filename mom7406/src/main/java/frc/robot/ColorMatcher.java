9878a3c18aa246c993fb4d6d85e726607b9bee54
Rishi Soni <rishisoni.5678@gmail.com>
2021-02-10 20:01:47 -0500

    Update ColorMatcher.java

--- mom7406/src/main/java/frc/robot/ColorMatcher.java
+++ mom7406/src/main/java/frc/robot/ColorMatcher.java
@@ -4,20 +4,20 @@ import com.revrobotics.*;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class ColorMatcher extends ColorMatch {
-    static final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-    static final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-    static final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-    static final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+    static final Color kBlueTarget = super.makeColor(0.143, 0.427, 0.429);
+    static final Color kGreenTarget = super.makeColor(0.197, 0.561, 0.240);
+    static final Color kRedTarget = super.makeColor(0.561, 0.232, 0.114);
+    static final Color kYellowTarget = super.makeColor(0.361, 0.524, 0.113);
 
     public void addColors() {
-        addColorMatch(kBlueTarget);
-        addColorMatch(kGreenTarget);
-        addColorMatch(kRedTarget);
-        addColorMatch(kYellowTarget);
+        super.addColorMatch(kBlueTarget);
+        super.addColorMatch(kGreenTarget);
+        super.addColorMatch(kRedTarget);
+        super.addColorMatch(kYellowTarget);
     }
 
     public String matchColor(Color inputtedRGB) {
-        ColorMatchResult match = matchClosestColor(detectedColor);
+        ColorMatchResult match = super.matchClosestColor(inputtedRGB);
 
         switch (match.color) {
             case kBlueTarget :
