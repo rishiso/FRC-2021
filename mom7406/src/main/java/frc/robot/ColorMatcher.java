4adbd4fa6c5b9ecb2fbaa815b96f29b6db5c2790
Rishi Soni <rishisoni.5678@gmail.com>
2021-02-11 16:19:18 -0500

    Update ColorMatcher.java

--- mom7406/src/main/java/frc/robot/ColorMatcher.java
+++ mom7406/src/main/java/frc/robot/ColorMatcher.java
@@ -4,10 +4,10 @@ import com.revrobotics.*;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class ColorMatcher extends ColorMatch {
-    static final Color kBlueTarget = super.makeColor(0.143, 0.427, 0.429);
-    static final Color kGreenTarget = super.makeColor(0.197, 0.561, 0.240);
-    static final Color kRedTarget = super.makeColor(0.561, 0.232, 0.114);
-    static final Color kYellowTarget = super.makeColor(0.361, 0.524, 0.113);
+    static final Color kBlueTarget = makeColor(0.143, 0.427, 0.429);
+    static final Color kGreenTarget = makeColor(0.197, 0.561, 0.240);
+    static final Color kRedTarget = makeColor(0.561, 0.232, 0.114);
+    static final Color kYellowTarget = makeColor(0.361, 0.524, 0.113);
 
     public void addColors() {
         super.addColorMatch(kBlueTarget);
@@ -16,25 +16,19 @@ public class ColorMatcher extends ColorMatch {
         super.addColorMatch(kYellowTarget);
     }
 
-    public String matchColor(Color inputtedRGB) {
+    public String colorCheck(Color inputtedRGB) {
         ColorMatchResult match = super.matchClosestColor(inputtedRGB);
 
-        switch (match.color) {
-            case kBlueTarget :
-                return("Blue");
-                break;
-            case kGreenTarget :
-                return("Green");
-                break;
-            case kRedTarget :
-                return("Red");
-                break;
-            case kYellowTarget :
-                return("Yellow");
-                break;
-            default :
-                return("L");
-                break;
-        }
+        if (match.color == kBlueTarget) {
+            return("Blue");
+          } else if (match.color == kRedTarget) {
+            return("Red");
+          } else if (match.color == kGreenTarget) {
+            return("Green");
+          } else if (match.color == kYellowTarget) {
+            return("Yellow");
+          } else {
+            return("Unknown");
+          }
     }
 }
