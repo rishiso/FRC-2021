305dd98c00a6847c856ca73cdac2d7056bce5a90
Rishi Soni <rishisoni.5678@gmail.com>
2021-09-13 11:49:13 -0400

    Update PullActuator.java

--- mom7406/src/main/java/frc/robot/PullActuator.java
+++ mom7406/src/main/java/frc/robot/PullActuator.java
@@ -8,22 +8,22 @@ public class PullActuator extends Servo {
     
     public PullActuator(Integer port) {
         super(port);
-        super.setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
+        setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
     }
 
     public void open() {
         mode = "OPEN";
-        super.setSpeed(1.0);
+        setSpeed(1.0);
     }
 
     public void close() {
         mode = "CLOSE";
-        super.setSpeed(-1.0);
+        setSpeed(-1.0);
     }
 
     public void stop() {
         mode = "IDLE";
-        super.stopMotor();
+        stopMotor();
     }
 
 }
